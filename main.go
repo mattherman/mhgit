ea8133be08ad2bd2cba370d8600dcb8af0bffe14
Matthew Herman <mattherman11@gmail.com>
2017-05-02 21:17:44 -0500

    Reading objects w/o separating headers

--- main.go
+++ main.go
@@ -4,12 +4,18 @@ import (
 	"bytes"
 	"compress/zlib"
 	"crypto/sha1"
-	"encoding/base64"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strconv"
 )
+
+type object struct {
+	data       []byte
+	objectType string
+}
 
 func main() {
 	args := os.Args[1:]
@@ -22,14 +28,18 @@ func main() {
 	switch args[0] {
 	case "init":
 		initializeRepo()
-		hashObject("abcdefg", "blob")
+	case "hash":
+		hashObject(object{data: []byte("what is up, doc?"), objectType: "blob"}, true)
+	case "read":
+		obj := readObject(args[1])
+		fmt.Printf("%s", obj.data)
 	}
 }
 
 func initializeRepo() {
 	if fileDoesNotExist("./.git") {
 		createInitialDirectoriesAndFiles()
-		fmt.Println("Initialized empty repository.")
+		fmt.Println("Initialized empty Git repository.")
 	} else {
 		fmt.Println("A git repository already exists in this directory.")
 	}
@@ -50,36 +60,88 @@ func fileDoesNotExist(path string) bool {
 	return os.IsNotExist(err)
 }
 
-func hashObject(data string, objectType string) {
-	header := []byte(objectType + " " + strconv.Itoa(len(data)))
-	fmt.Println(header)
+func hashObject(objectToHash object, write bool) string {
+	header := []byte(objectToHash.objectType + " " + strconv.Itoa(len(objectToHash.data)) + "\000")
 
-	fullData := append(header, 0)
-	fullData = append(fullData, data...)
-	fmt.Println(fullData)
+	fullData := append(header, objectToHash.data...)
 
 	sha1 := computeSha1(fullData)
 	fmt.Println(sha1)
 
-	objectPath := "./.git/objects/" + sha1[:2]
-	fmt.Println(objectPath)
+	objectPath := filepath.Join("./.git/objects", sha1[:2])
 
 	os.Mkdir(objectPath, 0700)
-	fileName := objectPath + "/" + sha1[2:]
+	fileName := filepath.Join(objectPath, sha1[2:])
 	fmt.Println(fileName)
 
-	var buf bytes.Buffer
-	w := zlib.NewWriter(&buf)
-	defer w.Close()
-	w.Write(fullData)
+	if write {
+		var buf bytes.Buffer
+		w := zlib.NewWriter(&buf)
+		w.Write(fullData)
+		w.Close()
 
-	bufBytes := buf.Bytes()
-	fmt.Println(bufBytes)
-	ioutil.WriteFile(fileName, buf.Bytes(), 0700)
+		ioutil.WriteFile(fileName, buf.Bytes(), 0700)
+	}
+
+	return sha1
 }
 
 func computeSha1(data []byte) string {
 	hasher := sha1.New()
 	hasher.Write(data)
-	return base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	hashBytes := hasher.Sum(nil)
+	return fmt.Sprintf("%x\n", hashBytes)
+}
+
+func readObject(hash string) object {
+	if len(hash) < 3 {
+		fmt.Println("Prefix provided must be at least three characters.")
+		return object{}
+	}
+
+	hashDirectory := "./.git/objects/" + hash[:2]
+	hashRemainder := hash[2:]
+
+	if fileDoesNotExist(hashDirectory) {
+		fmt.Println("Object " + hash + " not found.")
+		return object{}
+	}
+
+	files, _ := filepath.Glob(hashDirectory + "/" + hashRemainder + "*")
+	if len(files) == 0 {
+		fmt.Println("Object " + hash + " not found.")
+	} else if len(files) > 1 {
+		fmt.Println("Found multiple matches for " + hash + ".")
+	} else {
+		dat, _ := ioutil.ReadFile(files[0])
+
+		b := bytes.NewReader(dat)
+
+		r, err := zlib.NewReader(b)
+		if err != nil {
+			panic(err)
+		}
+		p, _ := ioutil.ReadAll(r)
+		fmt.Println(p)
+
+		r.Close()
+
+		return object{data: p, objectType: "blob"}
+	}
+
+	return object{}
+}
+
+func test() {
+	buff := []byte{120, 156, 202, 72, 205, 201, 201, 215, 81, 40, 207,
+		47, 202, 73, 225, 2, 4, 0, 0, 255, 255, 33, 231, 4, 147}
+	b := bytes.NewReader(buff)
+
+	r, err := zlib.NewReader(b)
+	if err != nil {
+		panic(err)
+	}
+	io.Copy(os.Stdout, r)
+
+	r.Close()
 }
