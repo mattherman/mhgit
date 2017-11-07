f2845f137ea22201d7e3717cfa4aeebd70ef925b
Matthew Herman <mattherman11@gmail.com>
2017-11-06 19:52:30 -0600

    Update command line args

--- main.go
+++ main.go
@@ -12,21 +12,24 @@ import (
 var (
 	app = kingpin.New("mhgit", "An implementation of Git written in Go.")
 
-	initialize = app.Command("init", "Initialize a new repository.")
+	initialize = app.Command("init", "Create an empty Git repository or reinitialize an existing one.")
 
-	hashObject      = app.Command("hash-object", "Hash an existing file.")
+	hashObject      = app.Command("hash-object", "Compute object ID and optionally creates a blob from a file.")
 	hashObjectWrite = hashObject.Flag("write", "Whether or not to write the object to the Git object store.").Short('w').Bool()
+	hashObjectType  = hashObject.Flag("type", "The type of object. Defaults to 'blob'.").Default("blob").Short('t').String()
 	hashObjectFile  = hashObject.Arg("file", "The path to the file being hashed.").Required().String()
 
-	catFile       = app.Command("cat-file", "Inspect a stored Git object.")
+	catFile       = app.Command("cat-file", "Provide content or type and size information for repository objects.")
 	catFilePretty = catFile.Flag("pretty", "Pretty-print the object based on type.").Short('p').Bool()
 	catFileType   = catFile.Flag("type", "Output the type of the object.").Short('t').Bool()
 	catFileSize   = catFile.Flag("size", "Output the size of the object.").Short('s').Bool()
-	catFileObject = catFile.Arg("object", "The name of the object to show.").Required().String()
+	catFileObject = catFile.Arg("object", "The hash of the object to show.").Required().String()
 )
 
 func main() {
+
 	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+
 	case initialize.FullCommand():
 		err := command.InitializeRepo()
 		if err != nil {
@@ -34,9 +37,11 @@ func main() {
 		} else {
 			fmt.Println("Initialized empty Git repository.")
 		}
+
 	case hashObject.FullCommand():
 		hash := command.HashObject(*hashObjectFile, *hashObjectWrite)
 		fmt.Println(hash)
+
 	case catFile.FullCommand():
 		obj, err := command.CatFile(*catFileObject)
 		if err != nil {
@@ -45,4 +50,5 @@ func main() {
 			fmt.Printf("%s : %s\n", obj.ObjectType, obj.Data)
 		}
 	}
+
 }
